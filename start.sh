8bd3ece11e71ba94fb40aa2022680046c954702c
Lucifer Morningstar <103210935+Lucifer86790@users.noreply.github.com>
2023-01-05 23:07:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/JayR1213/Ipopcornbot /Ipopcornbot
-cd /Ipopcornbot
+git clone https://github.com/Lucifer86790/420 /420
+cd /420
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
