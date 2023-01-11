4423b2685991434ef0120505ae4ae290c9d60480
Lightsays <112267654+Lightsays@users.noreply.github.com>
2023-01-12 00:16:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Lucifer86790/420 /420
-cd /420
+git clone https://github.com/Lucifer86790/search /search
+cd /search
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
